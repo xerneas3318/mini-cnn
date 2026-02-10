9e9142ecda7ac7cca11c35b822f6dd3a3eddafbe
Avinash <reachavinashs1@gmail.com>
2026-02-10 12:37:11 -0800

    working cnnv1

---
+++ mini-cnn.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+
+
+class ConvBlock(nn.Module):
+
+class DepthWiseBlock(nn.Module):
+
+class ResidualStack(nn.Module):
+
+
+class MiniCNN(nn.Module):
+    def __init__(self):
+        super(MiniCNN, self).__init__()
+
